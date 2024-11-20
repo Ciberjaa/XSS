33516e193fc517f3ecbca167072c5a6dd611ab7d
Ciberjaa <jonatan.aa104@gmail.com>
2024-11-20 09:14:24 +0100

    patata.js

--- patata.js
+++ patata.js
@@ -1,1 +1,14 @@
-alert('XSS');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejecutar JS</title>
+    <script>
+        alert('XSS');
+    </script>
+</head>
+<body>
+    <h1>JavaScript ejecutado</h1>
+</body>
+</html>
